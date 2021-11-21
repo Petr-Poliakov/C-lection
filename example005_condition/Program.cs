f278df890f4cf2e6d00297bd1287ec50af6adbed
Polyakov <Polyakov@MacBook-Pro.local>
2021-11-22 01:52:14 +0300

    add example005 if hello

---
+++ example005_condition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Ваше имя ");
+string username = Console.ReadLine();
+if (username == "Яна")
+{
+   Console.WriteLine("Ура! Это же ЯНА"); 
+} 
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine (username);
+}
